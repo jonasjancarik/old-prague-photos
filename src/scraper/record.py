d2f14e16e1c7df1a53f4e87da351d4d8dd26d307
Jonáš Jančařík <jonas.jancarik@gmail.com>
2023-11-06 21:23:42 +0100

    ensure output dir exists

--- src/scraper/record.py
+++ src/scraper/record.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import os
 from typing import Dict, Any
 
 
@@ -10,6 +11,8 @@ class Record:
 
     def save(self) -> None:
         output_filename = f"output/records/{self.xid}.json"
+        # create the directory if it doesn't exist
+        os.makedirs(os.path.dirname(output_filename), exist_ok=True)
         with open(output_filename, "w", encoding="utf-8") as f:
             json.dump(self.data, f, ensure_ascii=False)
         logging.info(f"Record {self.xid} saved.")
