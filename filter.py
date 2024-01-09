6596cf3176328fe5d9e4eade89cad2a51f6c6c5a
Jonáš Jančařík <jonas.jancarik@gmail.com>
2024-01-09 16:29:10 +0100

    Refactor filtering and processing of records, drop process.py

--- filter.py
+++ filter.py
@@ -1,11 +1,64 @@
 import json
 import os
+import time
 
-# load records from file
 records = []
 
-with open("output/records_with_places.json", "r", encoding="utf-8") as file:
-    records = json.load(file)
+PROCESS_RAW_RECORDS = True  # dev option, set to False if the records have already been processed once to save time and you're making changes to the filtering. For 10k records it takes around a minute or two to process them.
+
+if PROCESS_RAW_RECORDS:
+    # load all the records from the output/records directory - those are all JSON files
+    print(
+        "Loading scraped records from output/records and filtering out those without a place..."
+    )
+
+    file_list = os.listdir("output/records")
+
+    start_time = time.time()
+
+    for i, filename in enumerate(file_list):
+        if filename.endswith(".json"):
+            with open(f"output/records/{filename}", "r", encoding="utf-8") as file:
+                records.append(json.load(file))
+        percentage = (i + 1) / len(file_list) * 100
+        elapsed_time = time.time() - start_time
+        eta = elapsed_time / (i + 1) * (len(file_list) - i - 1)
+        print(
+            f"{percentage:.2f}% ({i+1}/{len(file_list)}) | {elapsed_time:.2f}s | ETA: {eta:.2f}s",
+            end="\r",
+        )
+
+    records_all_count = len(records)
+
+    print(f"\nLoaded {records_all_count} records.")
+
+    # drop all the records that don't have at least one key named "rejstříkové záznamy"."místo"
+    start_time = time.time()
+
+    for i, record in enumerate(records):
+        if not any(
+            "místo" in rejstrik["typ"].lower()
+            for rejstrik in record["rejstříkové záznamy"]
+        ):
+            records.pop(i)
+        elapsed_time = time.time() - start_time
+        eta = elapsed_time / (i + 1) * (records_all_count - i - 1)
+        print(
+            f"{i+1}/{records_all_count} | Elapsed time: {elapsed_time:.2f}s | ETA: {eta:.2f}s",
+            end="\r",
+        )
+
+    # save the records to a new file
+    with open("output/records_with_places.json", "w", encoding="utf-8") as file:
+        json.dump(records, file, ensure_ascii=False)
+
+else:
+    # load the records from the output/records_with_places.json file
+
+    with open("output/records_with_places.json", "r", encoding="utf-8") as file:
+        records = json.load(file)
+
+print(f"\nFound {len(records)} records with places.")
 
 # loop through all the records and find those that contain "č. p." or "č.p." in the "rejstříkové záznamy"."obsah" key and count them
 filtered_records = {
